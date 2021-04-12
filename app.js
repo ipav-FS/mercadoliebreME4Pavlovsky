295b3ad25cec8c670f2e668855809535d443c951
ipav-FS <ipavlovsky@gmail.com>
2021-04-12 17:15:33 -0300

    fix attempt

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3030
-const puerto = process.env.PORT ; 
+const express = require('express');
+const app = express();
 const path = require('path');
-console.log('Escuchando');
-app.use(express.static('public'));
 
+const publicPath = path.resolve(__dirname, './public') ;
+const puerto= process.env.PORT;
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/home.html'));
+app.use(express.static(publicPath));
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/home.html'));
 });
 
 app.get('/login', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/login.html'));
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
 
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname, './views/register.html'));
+app.get('/register', (req,res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
 });
 
-app.listen(puerto || 3030, function() {
-    console.log("Servidor corriendo en el puerto 3030");
+app.listen(puerto || 3000, function() {
+    console.log("Servidor corriendo en el puerto 3000");
 });
